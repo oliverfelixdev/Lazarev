d684020ffe7374c7229b1bcfbd0e2a9fb855906f
Oliver Felix <oliverfelix.dev@gmail.com>
2025-06-24 07:19:01 +0100

    responsive layouts updated

--- js/script.js
+++ js/script.js
@@ -232,12 +232,21 @@ reeller();
 let mfCursor = () => {
   const cursor = new MouseFollower();
   const vidWrapper = document.querySelector(".video-intro-wrapper");
+  const ftrContact = document.querySelector(".footer-contact h2");
 
   vidWrapper.addEventListener("mouseenter", () => {
     cursor.setSkewing(0.2);
   });
 
   vidWrapper.addEventListener("mouseleave", () => {
+    cursor.removeSkewing();
+  });
+
+  ftrContact.addEventListener("mouseenter", () => {
+    cursor.setSkewing(0.2);
+  });
+
+  ftrContact.addEventListener("mouseleave", () => {
     cursor.removeSkewing();
   });
 
